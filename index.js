9409199bb1e55a0169cfe5ca60525dd7653fcc63
albertdev26 <70877522+albertdev26@users.noreply.github.com>
2021-03-30 17:35:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node demo!\n'
+  const msg = 'Hello Node final test!\n'
   res.end(msg);
 });
 
